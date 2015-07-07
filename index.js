a4ebc0a304306838053dc3d50da9756f7abee3c2
Bradley Booth <bradley.booth@gmail.com>
2015-07-07 12:27:37 -0700

    Fix jquery easing file names after a breaking change by the jquery-easing-original bower repo

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 module.exports = {
   name: 'ember-cli-jquery-easing',
   included: function (app) {
-    app.import(app.bowerDirectory + "/jquery-easing-original/jquery.easing.1.3.js");
+    app.import(app.bowerDirectory + "/jquery-easing-original/jquery.easing.js");
   }
 };
